af2da29d1cbe82cac52ef11923dd2f97c7e4e98f
Aman Upadhyay <as5738864@gmail.com>
2026-07-13 01:09:28 +0530

    Time: 5 ms (44.26%) | Memory: 94.5 MB (44.84%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,19 +10,58 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ArrayList<Integer> list = new ArrayList();
-        ListNode temp = head;
-        while(temp!=null){
-            list.add(temp.val);
-            temp = temp.next;
+        // ArrayList<Integer> list = new ArrayList();
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     list.add(temp.val);
+        //     temp = temp.next;          // brute force
+        // }
+        // int i = 0;
+        // int j = list.size()-1;
+        // while(i<j){
+        //     if(list.get(i)!=list.get(j)) return false;
+        //     i++;
+        //     j--;
+        // }
+        // return true;
+
+         
+
+
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        int i = 0;
-        int j = list.size()-1;
-        while(i<j){
-            if(list.get(i)!=list.get(j)) return false;
-            i++;
-            j--;
-        }
-        return true;
+
+       ListNode head1 = slow.next;
+       slow.next = null;
+
+        
+       head1 = reverse(head1);
+       ListNode temp = head;
+       ListNode temp2 = head1;
+
+       while(temp2!=null){
+        if(temp.val != temp2.val) return false;
+        temp = temp.next;
+        temp2 = temp2.next;
+       }  
+       return true;  
+
     }
+    ListNode reverse(ListNode head){
+        ListNode cur = head;
+        ListNode pre = null;
+        ListNode forw = null;
+        while(cur!=null){
+            forw = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = forw;
+        }
+        return pre;
+
+       }
 }
